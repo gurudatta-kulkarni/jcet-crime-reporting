bc461f51ad32507a75e5f2ad0a33aba09f64e80e
gurudatta-kulkarni <65105495+gurudatta-kulkarni@users.noreply.github.com>
2023-07-02 07:38:08 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+global $conn;
+  $conn = new PDO("mysql:host=$servername;dbname=ocrs_db", $username, $password);
+
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  if ($conn) {
+  // echo "datababe is Connected successfully";
+
+  }else{
+  echo "Connection failed";
+  }
+
+?>
